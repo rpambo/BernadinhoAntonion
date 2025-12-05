097e8c12565422ba4a6e81774bf16fdab497069d
Rafael Pambo <rafaelgeniokitoco@gmail.com>
2025-12-05 18:45:36 +0100

    sobre nos

---
+++ bernadinhoAntonio/src/app/components/back/back.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-back',
+  imports: [],
+  templateUrl: './back.html',
+  styleUrl: './back.css',
+})
+export class Back {
+
+}
--- bernadinhoAntonio/src/app/pagina/home/home.ts
+++ bernadinhoAntonio/src/app/pagina/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SlideLogo } from '../../components/slide-logo/slide-logo';
 import { SobreBernadino } from '../../components/sobre-bernadino/sobre-bernadino';
+import { Back } from "../../components/back/back";
 
 @Component({
   selector: 'app-home',
-  imports: [SlideLogo, SobreBernadino],
+  imports: [SlideLogo, SobreBernadino, Back],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
