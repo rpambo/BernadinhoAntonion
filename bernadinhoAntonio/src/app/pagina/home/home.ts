21920d8dcf209d5ab2353645a409ad56db682e07
Rafael Pambo <rafaelgeniokitoco@gmail.com>
2025-12-10 18:42:40 +0100

    finsh

--- bernadinhoAntonio/src/app/pagina/home/home.ts
+++ bernadinhoAntonio/src/app/pagina/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { SlideLogo } from '../../components/slide-logo/slide-logo';
 import { SobreBernadino } from '../../components/sobre-bernadino/sobre-bernadino';
 import { Back } from "../../components/back/back";
+import { Footer } from "../../components/footer/footer";
 
 @Component({
   selector: 'app-home',
-  imports: [SlideLogo, SobreBernadino, Back],
+  imports: [SlideLogo, SobreBernadino, Back, Footer],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
